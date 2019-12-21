9e6ee59063afa9e2573ae6ba25555c3a271fac34
wq12 <1742164588@qq.com>
2019-12-21 14:27:28 +0800

    修改了helloworld 函数的结果

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 
 from flask import Flask
+from flask import render_template
 
 app=Flask(__name__)
 
+
 @app.route('/')
 def  hello():
-    return 'hello world'
+    return render_template('home.html')
 
 
 if __name__ == '__main__':
     app.debug=True
     app.run()
+
 
