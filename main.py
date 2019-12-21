fcdf0a729007f853fde15a9ce1294d742e7b6466
wq12 <1742164588@qq.com>
2019-12-21 17:25:16 +0800

    添加了 gitignore

--- main.py
+++ main.py
@@ -3,11 +3,14 @@
 from flask import Flask
 from flask import render_template
 
+from funcs import add
+
 app=Flask(__name__)
 
 
 @app.route('/')
 def  hello():
+    add(9,7)
     return render_template('home.html')
 
 
